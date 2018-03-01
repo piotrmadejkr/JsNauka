1a766a29e95f7e3f3076763aeff00c5eac732b6c
Piotr Madej <piotr.madejkr@gmail.com>
2018-03-01 12:51:29 +0100

    add js exercise 8,5

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,9 @@
 // scripts.js
 
+var a = 5;
+var h = 4;
+var triangleArea;
+
+triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+
